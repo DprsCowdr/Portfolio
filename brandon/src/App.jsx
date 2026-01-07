c4499c600bb4b0f6d6e3227d0fff170d2e2a9a99
Brandon <caritosbrandon@gmail.com>
2026-01-07 23:42:43 +0800

    done

--- brandon/src/App.jsx
+++ brandon/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from "./pages/home"
-import { Notfound } from "./pages/notfound"
+import { Home } from "./pages/Home"
+import { Notfound } from "./pages/Notfound"
 function App() {
 
 
